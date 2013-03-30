ad5483390edd5d4422d68817aef98681f5dbc521
hmf <hmf@qint.de>
2013-03-30 08:23:31 +0100

    Funql Runner Gui load and save

--- ggui/src/main/kotlin/org/funql/ri/gui/view.kt
+++ ggui/src/main/kotlin/org/funql/ri/gui/view.kt
@@ -5,6 +5,9 @@ import kotlin.swing.*
 import java.awt.Desktop
 import java.net.URI
 import java.awt.event.ActionEvent
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.FileWriter
 
 fun main(args: Array<String>): Unit {
     val v = View()
@@ -16,40 +19,67 @@ fun main(args: Array<String>): Unit {
 }
 class View()
 {
-        val funqlEdit = JTextArea("Edit")
-        val funqlResult = JTextArea("Result")
+    val funqlEdit = JTextArea("Edit")
+    val funqlResult = JTextArea("Result")
 
-        val theFrame = frame("Funql Runner") {
-            exitOnClose()
+    val theFrame = frame("Funql Runner") {
+        exitOnClose()
 
-            jmenuBar = menuBar{
-                menu("File") {
-                    add(action("Open") { openFile(it)})
-                    add(action("Save") { println("Show about window") })
-                    add(action("New") { println("Show about window") })
-                    add(action("Connect") { println("Show about window") })
-                }
-                menu("Help") {
-                    add(action("Web Help"){
-                        val desktop = if (Desktop.isDesktopSupported()) Desktop.getDesktop() else throw Error("Desktop access not possible to launch web help");
-                            try {
-                                desktop.browse(URI("http://www.funql.org/funqlrunner/help"));
-                            } catch (e: Exception) {
-                                e.printStackTrace();
-                            }
-                        })
-                }
+        jmenuBar = menuBar{
+            menu("File") {
+                add(action("Open") { openFile(it) })
+                add(action("Save") { saveFile(it) })
+                add(action("New") { println("Show about window") })
+                add(action("Connect") { println("Show about window") })
             }
-            center = JSplitPane(JSplitPane.VERTICAL_SPLIT, funqlEdit, funqlResult)
+            menu("Help") {
+                add(action("Web Help") {
+                    val desktop = if (Desktop.isDesktopSupported()) Desktop.getDesktop() else throw Error("Desktop access not possible to launch web help");
+                    try {
+                        desktop.browse(URI("http://www.funql.org/funqlrunner/help"));
+                    } catch (e: Exception) {
+                        e.printStackTrace();
+                    }
+                })
+            }
+        }
+        center = JSplitPane(JSplitPane.VERTICAL_SPLIT, funqlEdit, funqlResult)
     }
 
 
-    fun openFile(e: ActionEvent): Unit{
+    fun openFile(e: ActionEvent): Unit {
         val fc = JFileChooser()
         val ret = fc.showOpenDialog(theFrame)
         if (ret == JFileChooser.APPROVE_OPTION)
+        {
+            val file = fc.getSelectedFile()
+            if (file != null)
             {
-                funqlEdit.setText(fc.getSelectedFile()?.getAbsolutePath())
+                val r = FileReader(file)
+                val s = r.readText()
+
+                funqlEdit.setText(s)
+                r.close()
             }
+        }
+    }
+    fun saveFile(e: ActionEvent): Unit {
+        val fc = JFileChooser()
+        val ret = fc.showSaveDialog(theFrame)
+        if (ret == JFileChooser.APPROVE_OPTION)
+        {
+            val file = fc.getSelectedFile()
+            if (file == null)
+                return
+            var w: FileWriter? = null
+            try
+            {
+                w = FileWriter(file)
+                w?.write(funqlEdit.getText()!!)
+            }
+            finally {
+                w?.close()
+            }
+        }
     }
 }
