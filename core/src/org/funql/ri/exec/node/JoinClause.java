8995bcd287ba5d034c2092f2c69718001f2974ca
hmf <hmf@qint.de>
2014-07-17 22:25:22 +0200

    parse join 0.2

--- core/src/org/funql/ri/exec/node/JoinClause.java
+++ core/src/org/funql/ri/exec/node/JoinClause.java
@@ -1,32 +1,32 @@
 package org.funql.ri.exec.node;
 import org.funql.ri.data.FqlDataException;
 import org.funql.ri.data.FqlIterator;
+import org.funql.ri.data.FunqlConnection;
 import org.funql.ri.exec.FqlStatement;
 import org.funql.ri.exec.RunEnv;
-import org.funql.ri.exec.node.EntryPointSlot;
-import org.funql.ri.exec.node.FqlNodeInterface;
 
 /**
  * Created by hmf on 17.07.2014.
  */
 public class JoinClause  implements FqlStatement
 {
-    private final String root;
+    private final String containerName;
     private final String alias;
-    private final EntryPointSlot entryPointSlot;
+    private final EntryPointSlot connectionSlot;
     private final FqlNodeInterface joinExpression;
 
-    public JoinClause(String root, String alias, EntryPointSlot entryPointSlot, FqlNodeInterface joinExpression)
+    public JoinClause(String containerName, String alias, EntryPointSlot connectionSlot, FqlNodeInterface joinExpression)
     {
-        this.root = root;
+        this.containerName = containerName;
         this.alias = alias;
-        this.entryPointSlot = entryPointSlot;
+        this.connectionSlot = connectionSlot;
         this.joinExpression = joinExpression;
     }
 
-    @Override
-    public FqlIterator execute(RunEnv env, FqlIterator precedent) throws FqlDataException
-    {
-        return null;
+    public FqlIterator execute(RunEnv env, FqlIterator precedent) throws FqlDataException {
+        FunqlConnection funqlConnection = env.getConnection(connectionSlot.getIndex());
+        FqlIterator listContainer = funqlConnection.getIterator(containerName);
+        // if joinExpression is equiJoin process hash join otherwise full product
+        return listContainer;
     }
 }
